f50618256f36afff4f70072da098ea39b32c48b6
KatiaDev <ecaterina.popa.2021@gmail.com>
2021-10-16 16:37:19 +0300

    Initialize project

---
+++ Hobbits/hobbits.js
@@ -0,0 +1,11 @@
+const hobbits = [
+  { name: "Frodo Baggins" },
+  { name: "Samwise Gamgee" },
+  { name: "Meriadoc Brandybuck" },
+  { name: "Peregrin Took" },
+  { name: "Mithrandir" },
+  { name: "Boromir" },
+  { name: "Legolas" },
+  { name: "Gimli" },
+  { name: "Aragorn" },
+];
---
+++ Movies/movies.js
@@ -0,0 +1,54 @@
+const movies = [
+  {
+    id: "60cf67601fed544ab437a520",
+    genre: ["Acțiune", "Aventuri", "Crimă", "Thriller"],
+    actors: ["Vin Diesel", "Michelle Rodrigez", "Jordana Brewster"],
+    title: "Furios și iute 9",
+    director: "Justin Lin",
+    release_date: "2021-05-11",
+    rating: 5.2,
+    description:
+      "Dom Toretto duce o viață liniștită alături de Letty și fiul său, micuțul Brian, cu toate că pericolul planează întotdeauna asupra orizontului lor pașnic. De data aceasta, această amenințare îl va forța pe Dom să-și înfrunte greșelile trecutului, pentru a-i putea salva pe cei pe care îi iubește cel mai mult. Echipajul său se reunește pentru a opri un complot periculos condus de cel mai priceput asasin și șofer profesionist pe care l-au întâlnit vreodată: un bărbat care se întâmplă să fie și fratele abandonat al lui Dom, Jakob. Pe parcurs, vechi prietenii vor fi readuse la viață, vechi dușmani se vor întoarce, istoria va fi rescrisă, iar adevăratul sens al familiei va fi testat mai mult ca niciodată.",
+    age_restriction: "AP-12",
+    duration: "2h 25min",
+    image_url:
+      "https://res.cloudinary.com/olymp-cinema/image/upload/v1624205056/movies/tbbnnshjv7dfxosejumr.jpg",
+    vodeo_url:
+      "https://res.cloudinary.com/olymp-cinema/video/upload/v1624205079/movies/j9ggmbwon3phtpcrivr1.mp4",
+  },
+  {
+    id: "60cf69e21fed544ab437a521",
+    genre: ["Acțiune", "Crimă", "Comedie"],
+    actors: ["Ryan Reynolds", "Salma Hayek", "Samuel L. Jackson"],
+    title: "Hitman's Bodyguard 2: Nevasta asasinului",
+    director: "Patrick Hughes",
+    release_date: "2021-06-02",
+    rating: 6.5,
+    description:
+      "Cel mai letal din lume cuplu ciudat - bodyguardul Michael Bryce (Ryan Reynolds) și asasinul Darius Kincaid (Samuel L. Jackson) - se întorc cu o altă misiune care le pune viața în pericol. Tot fără licență, dar relaxat în concediu, Bryce este forțat să intre în acțiune de către soția și mai instabilă a lui Darius, infama escroacă internațională Sonia Kincaid (Salma Hayek). Astfel, Bryce e aruncat în haos de către periculoșii lui protejați, și trio-ul criminal intră într-un complot global pentru a descoperi rapid că ei sunt singurii care pot împiedica dezastrul total în Europa.",
+    age_restriction: "IM-18",
+    duration: "1h 39min",
+    image_url:
+      "https://res.cloudinary.com/olymp-cinema/image/upload/v1624205749/movies/wq58dzwq5blg9bdcecoz.jpg",
+    vodeo_url:
+      "https://res.cloudinary.com/olymp-cinema/video/upload/v1624205774/movies/es6gsjvkbplqhvizdfnd.mp4",
+  },
+
+  {
+    id: "60cf6c541fed544ab437a522",
+    genre: ["Animație", "Aventuri", "Comedie"],
+    actors: ["Rose Byrne", "Domhnall Gleeson", "David Oyelowo"],
+    title: "Peter Iepuraşul: Fugit de acasă",
+    director: "Will Gluck",
+    release_date: "2021-06-01",
+    rating: 6.2,
+    description:
+      "Peter Rabbit, adaptarea pentru marele ecran a clasicei poveşti semnate de Beatrix Potter, revine pe ecrane cu un nou capitol. Deja știm că familia iepurească, în frunte cu ștrengarul Peter, se instalase în grădina conacului lui Thomas McGregor și a proaspetei sale soții, Bea. Deși au stabilit un armistițiu și Thomas s-a împăcat cu ideea că are iepuri în grădină, ba i-a mai și “adoptat”, cu condiția să-i lase în pace premiatele tomate, Peter nu se lasă de pozne cu una, cu două",
+    age_restriction: "AG",
+    duration: "1h 33min",
+    image_url:
+      "https://res.cloudinary.com/olymp-cinema/image/upload/v1624206386/movies/uavgot58fkszbyxqbset.jpg",
+    vodeo_url:
+      "https://res.cloudinary.com/olymp-cinema/video/upload/v1624206410/movies/yzprragwvf3gtjkjmwyh.mp4",
+  },
+];
