493d2b0a59fd3608f3693c5384df55876b3b98d7
KatiaDev <ecaterina.popa.2021@gmail.com>
2021-11-20 00:54:21 +0200

    Added Jobs resource

---
+++ Jobs/jobs.js
@@ -0,0 +1,63 @@
+[
+  {
+    id: 0,
+    jobTitle: "Senior Frontend Engineer",
+    company: "Peloton",
+    info: "Peloton is a global interactive fitness platform that brings the energy and benefits of studio-style workouts to the convenience and comfort of home.",
+    location: "New York City, NY, US",
+    experience: "5+",
+    salary: "Not disclosed",
+    description:
+      "You will be a senior engineer on the team that creates and expands the Peloton ecosystem on the web. You and your team will be working on the front lines to implement a intricate user experience that can be seamless for both Mobile and Web users. These products include our E-comm and Marketing experience, as well as a scalable web tool for our active users to engage with each other off of our hardware.",
+    website: "https://www.onepeloton.com/",
+  },
+
+  {
+    id: 1,
+    jobTitle: "Senior Android Engineer",
+    company: "Peloton",
+    info: "Peloton is a global interactive fitness platform that brings the energy and benefits of studio-style workouts to the convenience and comfort of home.",
+    location: "New York City, NY, US",
+    experience: "5+",
+    salary: "$120 000",
+    description:
+      "You will be working on the flagship Peloton experience that comes with every Peloton Bike and Tread. You will get the opportunity to work with our very own Android based Operating System, developing customer-facing features in Kotlin and Java, to deliver best-in-class fitness experiences across several different markets.",
+    website: "https://www.onepeloton.com/",
+  },
+  {
+    id: 2,
+    jobTitle: "Backend Engineer",
+    company: "Ascend.io",
+    info: "Imagine a data-driven world beyond even today's excitement around smart watches, smart homes, and smart cars. We'd like to help companies get there.",
+    location: "Palo Alto, CA, US",
+    experience: "4+",
+    salary: "$120000 - $160000",
+    description:
+      "Work on multiple components of a very powerful ‘big data engine’Code: today we do most of our work in Scala, Go and JavaScript (Node.js), with some Python and Java here and there. Why these languages? In short, we like them and we’ve found that they're among the best tools for data pipelines, backend, and frontend development, respectively. We could have just as easily picked other languages and frameworks, but so far are quite happy with these.",
+    website: "https://www.ascend.io/",
+  },
+  {
+    id: 3,
+    jobTitle: "Senior Software Engineer",
+    company: "Foursquare",
+    info: "Imagine a data-driven world beyond even today's excitement around smart watches, smart homes, and smart cars. We'd like to help companies get there.",
+    location: "Multiple Locations",
+    experience: "6+",
+    salary: "$160000",
+    description:
+      "Foursquare is the leading independent location technology platform, powering business solutions and consumer products through a deep understanding of location. Foursquare’s business solutions include Pilgrim SDK, Places API, Analytics, Placed powered by Foursquare, and Pinpoint. Together, these products empower brands to analyze trends; measure foot traffic lift; optimize advertising campaigns; and drive deeper engagement via Foursquare’s industry-leading developer tools, which have been selected by 150,000 developers including AccuWeather, Apple, Samsung, Microsoft, Snapchat, Tinder, TripAdvisor, Twitter and Uber.",
+    website: "https://foursquare.com/careers/",
+  },
+  {
+    id: 4,
+    jobTitle: "Engineering Manager, Desktop Firefox Frontend",
+    company: "Mozilla",
+    info: "Improving lives, not profits. When you join Mozilla, you’re becoming part of a global community that’s helping to build a brighter future for the Web — and for everyone.    ",
+    location: "San Francisco",
+    experience: "5+",
+    salary: "$160000",
+    description:
+      "The desktop Firefox team crafts a wide cross section of Firefox’s user interface. We’ve built upon the success of Firefox Quantum’s visual redesign by continually improving UI performance, experimenting with new search features, and modernizing our UI architecture. We also work closely with other teams building UI features. In this role you’ll have the opportunity to lead a team involved in a variety of projects with teams across Mozilla.",
+    website: "https://careers.mozilla.org/",
+  },
+];
